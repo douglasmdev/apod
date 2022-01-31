f0d8ad707b39d08b8ed49ecaee4fdc97fbeb2640
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-31 16:35:10 -0300

    date now defaults to current day

--- js/userDateInput.js
+++ js/userDateInput.js
@@ -2,11 +2,15 @@ const dateInput = $('#date-input')[0];
 
 const date = new Date();
 const currentYear = date.getFullYear();
-const currentMonth = date.getMonth() + 1;
+let currentMonth = date.getMonth() + 1;
+if (currentMonth < 10)
+    currentMonth = `0${currentMonth}`;
 const currentDay = date.getDate();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 
+
 dateInput.setAttribute('max', currentDate);
+dateInput.setAttribute('value', currentDate);
 
 let userDate = dateInput.value;
 
