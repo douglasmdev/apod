43d8ea6f91ea0b277f2d1bc76218e6faa3e369c9
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-31 16:11:10 -0300

    set max date attribute as today

--- js/userDateInput.js
+++ js/userDateInput.js
@@ -1,5 +1,13 @@
-
 const dateInput = $('#date-input')[0];
+
+const date = new Date();
+const currentYear = date.getFullYear();
+const currentMonth = date.getMonth() + 1;
+const currentDay = date.getDate();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+dateInput.setAttribute('max', currentDate);
+
 let userDate = dateInput.value;
 
 dateInput.onchange = function () {
