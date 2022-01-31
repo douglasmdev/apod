7807e91b408fb7fca2fa2b83e6bb41b29899a76e
Douglas Montes <douglasmontes.dev@gmail.com>
2022-01-31 15:53:57 -0300

    added video functionality

--- js/displayInfoOnPage.js
+++ js/displayInfoOnPage.js
@@ -2,10 +2,32 @@
 const resultField = $('#result-field')[0];
 const resultTitleField = $('#result-title')[0];
 const resultImgField = $('#result-image')[0];
+const resultVidField = $('#result-video')[0];
 const resultExpField = $('#result-explanation')[0];
 
+function isVideo(requestResult) {
+    if (requestResult.media_type == 'video')
+        return true;
+
+    return false;
+}
+
+function displayMedia(requestResult) {
+    if (!isVideo(requestResult)) {
+        $('#result-video').hide();
+        $('#result-image').show();
+        resultImgField.setAttribute('src', requestResult.hdurl);
+    } else {
+        $('#result-image').hide();
+        $('#result-video').show();
+        resultVidField.setAttribute('src', requestResult.url);
+    }
+        
+}
+
 function displayInfo(requestResult) {
+    $(resultImgField).empty();
     resultTitleField.innerText = requestResult.title;
-    resultImgField.setAttribute('src', requestResult.hdurl);
+    displayMedia(requestResult);
     resultExpField.innerText = requestResult.explanation;
 }
